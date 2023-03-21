e9b8db367ab37f409633cee84c20e349d380d1e8
DoveDev <dovedev@gmail.com>
2023-03-21 13:46:24 +0000

    Multiple Fixes
    Rectified miss spelt command Get-NetIPAddress (I incorrectly had used Get-NicIPAddress).
    Fixed the For loops and their priority for Alt2NicObjects and added Alt3NicObjects
    Added switch options for the the table output format (-t) and expanding the default visible NICs (-a) as a show all option.

--- Get-PrimaryIPv4Information.psm1
+++ Get-PrimaryIPv4Information.psm1
@@ -1,3 +1,5 @@
+Import-Module -Name NetAdapter
+Import-Module -Name NetTCPIP
 
 function ConvertFromIPToINT64(){ 
   param ($ip) 
@@ -37,95 +39,123 @@ function Get-IPrange(){
 }
 
 function Get-PrimaryIPv4Information(){
-  $NicObjects = Get-NetIPAddress | Where-Object {
-      ($_.InterfaceAlias -notlike 'vEthernet*') -and 
-      ($_.InterfaceAlias -notlike 'Loopback*') -and
-      ($_.InterfaceAlias -notlike 'Npcap*') -and
+  [CmdletBinding()]
+    param(
+        [Parameter()]
+        [switch]$t,
+
+        [Parameter()]
+        [switch]$a
+    )
+  if ($a){
+    $NicObjects = Get-NetIPAddress | Where-Object {
       ($_.PrefixLength -lt 64)
-  } | Select-Object InterfaceIndex,InterfaceAlias,MacAddress,IPv4Address,PrefixLength,SubnetMask,IPv4DefaultGateway,IPNetwork,SuffixOrigin,AddressState,FirstHost,LastHost
+    } | Select-Object InterfaceIndex,InterfaceAlias,MacAddress,IPv4Address,PrefixLength,SubnetMask,IPv4DefaultGateway,IPNetwork,SuffixOrigin,AddressState,FirstHost,LastHost
 
-  $AltNicObjects = Get-NicAdapter # No InterfaceIndex
-  Foreach ($AltNicObject in $AltNicObjects){
-      Foreach ($NicObject in $NicObjects){
-          if ($NicObject.IPv4Address -eq $($AltNicObject.Address.IPAddressToString)){
-              $NicObject.SubnetMask = $AltNicObject.IPv4Mask.IPAddressToString
-
-              $IPAddress = $NicObject.IPv4Address
-              $IPAddressArray = $IPAddress -split '\.' | Foreach-Object{[System.Convert]::ToString($_)}
-              #$IPAddressArray -join ' ' | Write-Host
-
-              $SubnetMask = $NicObject.SubnetMask
-              $SubnetMaskArray = $SubnetMask -split '\.' | Foreach-Object{[System.Convert]::ToString($_)}
-              $SubnetMaskIP = [Net.IPAddress]::Parse($SubnetMask)
-
-              #$SubnetMaskArray -join ' ' | Write-Host
-
-              $IPNetwork = '0','0','0','0'
-              Foreach ($IPAOctet in $IPAddressArray){
-              $i = $IPAddressArray.IndexOf($IPAOctet)
-              $IPNetwork[$i] = ($IPAOctet -band $SubnetMaskArray[$i])
-              }
-              #$IPNetwork -join ' ' | Write-Host
-              $NicObject.IPNetwork = $IPNetwork -join '.' | Write-Output
-              $IPNetworkIP = [Net.IPAddress]::Parse($NicObject.IPNetwork)
-              
-              $FirstHostOctetsI64Array = $IPNetwork | Foreach-Object{[System.Convert]::ToInt64($_)}
-              #$FirstHostOctetsArray | Write-Host
-              $FirstHostI64 = [int64]([int64]$FirstHostOctetsI64Array[0]*16777216 +
-                                      [int64]$FirstHostOctetsI64Array[1]*65536 +
-                                      [int64]$FirstHostOctetsI64Array[2]*256 +
-                                      ([int64]$FirstHostOctetsI64Array[3]+1))
-
-              #Write-Host "Debug : FirstHostI64 ="$FirstHostI64
-              <#$FirstHostIP = (([math]::truncate($FirstHostI64/16777216)).tostring()+"."+
-                              ([math]::truncate(($FirstHostI64%16777216)/65536)).tostring()+"."+
-                              ([math]::truncate(($FirstHostI64%65536)/256)).tostring()+"."+
-                              ([math]::truncate($FirstHostI64%256)).tostring())#>
-              $FirstHostIP = ConvertFromINT64ToIP($FirstHostI64)
-
-              #Write-Host "Debug : FirstHostIP ="$FirstHostIP
-              $NicObject.FirstHost = $FirstHostIP | Write-Output
-              
-              $NetworkBroadcastIPv4Address = new-object net.ipaddress (([system.net.ipaddress]::parse("255.255.255.255").address -bxor $SubnetMaskIP.address -bor $IPNetworkIP.address))
-              #Write-Host "Debug : NetworkBroadcastIPv4Address ="$NetworkBroadcastIPv4Address
-              $NetworkBroadcastIPv4AddressString = [System.Convert]::ToString($NetworkBroadcastIPv4Address)
-              #Write-Host "Debug : NetworkBroadcastIPv4AddressString ="$NetworkBroadcastIPv4AddressString
-              $LastHostOctetsArray = $NetworkBroadcastIPv4AddressString -split "\." | Foreach-Object{[System.Convert]::ToInt64($_)}
-              #Write-Host "Debug : LastHostOctetsArray ="$LastHostOctetsArray
-              $LastHostI64 = [int64]([int64]$LastHostOctetsArray[0]*16777216 +
-                              [int64]$LastHostOctetsArray[1]*65536 +
-                              [int64]$LastHostOctetsArray[2]*256 +
-                              ([int64]$LastHostOctetsArray[3]-1))
-                              
-              #Write-Host "Debug : LastHostI64 ="$LastHostI64
-              <#$LastHostIP = (([math]::truncate($LastHostI64/16777216)).tostring()+"."+
-                              ([math]::truncate(($LastHostI64%16777216)/65536)).tostring()+"."+
-                              ([math]::truncate(($LastHostI64%65536)/256)).tostring()+"."+
-                              ([math]::truncate($LastHostI64%256)).tostring())#>
-              
-              $LastHostIP = ConvertFromINT64ToIP($LastHostI64)
-              #Write-Host "Debug : LastHostIP ="$LastHostIP
-              $NicObject.LastHost = $LastHostIP | Write-Output
-          }
-      }
+    $Alt2NicObjects = Get-NetIPConfiguration
+    $Alt3NicObjects = [System.Net.NetworkInformation.NetworkInterface]::GetAllNetworkInterfaces() | 
+      Where-Object { ($_.OperationalStatus -eq 'Up')}
   }
+  else {
+    $NicObjects = Get-NetIPAddress | Where-Object {
+        ($_.InterfaceAlias -notlike 'vEthernet*') -and 
+        ($_.InterfaceAlias -notlike 'Loopback*') -and
+        ($_.InterfaceAlias -notlike 'Npcap*') -and
+        ($_.InterfaceAlias -notlike 'Local Area Connection*') -and
+        ($_.PrefixLength -lt 64)
+    } | Select-Object InterfaceIndex,InterfaceAlias,MacAddress,IPv4Address,PrefixLength,SubnetMask,IPv4DefaultGateway,IPNetwork,SuffixOrigin,AddressState,FirstHost,LastHost
 
-  $Alt2NicObjects = Get-NetIPConfiguration
-  Foreach ($Alt2NicObject in $Alt2NicObjects){
-      Foreach ($NicObject in $NicObjects){
-          if ($NicObject.InterfaceIndex -eq $Alt2NicObject.InterfaceIndex){
-              $IPv4DefaultGateway = Get-NetRoute | Where-Object -FilterScript {$_.ifIndex -eq $NicObject.InterfaceIndex} | Where-Object -FilterScript {$_.NextHop -Ne "::"} | Where-Object -FilterScript { $_.NextHop -Ne "0.0.0.0" }  | Where-Object -FilterScript { ($_.NextHop.SubString(0,6) -Ne 
-                  "fe80::") } | Select-Object NextHop
-              $NicObject.IPv4DefaultGateway = $IPv4DefaultGateway.NextHop
-
-              $MacAddress = Get-NetAdapter | Where-Object -FilterScript {$_.ifIndex -eq $NicObject.InterfaceIndex} | Select-Object MacAddress
-              $NicObject.MacAddress = $MacAddress -replace '.*=' -replace '}.*'
-          }
-      }
+    $Alt2NicObjects = Get-NetIPConfiguration
+    $Alt3NicObjects = [System.Net.NetworkInformation.NetworkInterface]::GetAllNetworkInterfaces() | 
+    Where-Object { ($_.OperationalStatus -eq 'Up') -and
+            ($_.Name -notlike '*vEthernet*') -and
+            ($_.Name -notlike '*Loopback*') -and
+            ($_.Name -notlike '*Npcap*') -and
+            ($_.Name -notlike '*Local Area Connection*')}
   }
+  
+  Foreach ($Alt3NicObject in $Alt3NicObjects){
+    [System.Net.NetworkInformation.IPInterfaceProperties] $adapterProperties  = $Alt3NicObject.GetIPProperties()
+    Foreach ($NicObject in $NicObjects){
+      Foreach ($unicastAddress in $adapterProperties.UnicastAddresses){
+        if (($unicastAddress.PrefixLength -lt 64) -and ($unicastAddress.Address.IPAddressToString -eq $NicObject.IPv4Address)){
+          #Obtain Subnet Mask
+          $SubnetMask = $unicastAddress.IPv4Mask.IPAddressToString
+          $NicObject.SubnetMask = $SubnetMask
+          $SubnetMaskIP = [Net.IPAddress]::Parse($NicObject.SubnetMask)
+          #Obtain IPv4 IP Address
+          $IPAddress = $NicObject.IPv4Address
+          $IPAddressArray = $IPAddress -split '\.' | Foreach-Object{[System.Convert]::ToString($_)}
+          $SubnetMaskArray = $SubnetMask -split '\.' | Foreach-Object{[System.Convert]::ToString($_)}
+          #Calculate IP Network
+          $IPNetwork = '0','0','0','0'
+          Foreach ($IPAOctet in $IPAddressArray){
+            $i = $IPAddressArray.IndexOf($IPAOctet)
+            $IPNetwork[$i] = ($IPAOctet -band $SubnetMaskArray[$i])
+          }
+          $NicObject.IPNetwork = $IPNetwork -join '.' | Write-Output
+          $IPNetworkIP = [Net.IPAddress]::Parse($NicObject.IPNetwork)
+          #Calculate First Host in Subnet
+          $FirstHostOctetsI64Array = $IPNetwork | Foreach-Object{[System.Convert]::ToInt64($_)}
+          #$FirstHostOctetsArray | Write-Host
+          if($NicObject.PrefixLength -ne 32){
+            $FirstHostI64 = [int64]([int64]$FirstHostOctetsI64Array[0]*16777216 +
+                                    [int64]$FirstHostOctetsI64Array[1]*65536 +
+                                    [int64]$FirstHostOctetsI64Array[2]*256 +
+                                    ([int64]$FirstHostOctetsI64Array[3]+1))
+          }
+          else {
+            $FirstHostI64 = [int64]([int64]$FirstHostOctetsI64Array[0]*16777216 +
+                                    [int64]$FirstHostOctetsI64Array[1]*65536 +
+                                    [int64]$FirstHostOctetsI64Array[2]*256 +
+                                    ([int64]$FirstHostOctetsI64Array[3]))
+          }             
+          $FirstHostIP = ConvertFromINT64ToIP($FirstHostI64)
+          $NicObject.FirstHost = $FirstHostIP | Write-Output
+          #Calculate Last Host in Subnet
+          $NetworkBroadcastIPv4Address = new-object net.ipaddress (([system.net.ipaddress]::parse("255.255.255.255").address -bxor $SubnetMaskIP.address -bor $IPNetworkIP.address))
+          $NetworkBroadcastIPv4AddressString = [System.Convert]::ToString($NetworkBroadcastIPv4Address)
+          $LastHostOctetsArray = $NetworkBroadcastIPv4AddressString -split "\." | Foreach-Object{[System.Convert]::ToInt64($_)}
+          if($NicObject.PrefixLength -ne 32){
+            $LastHostI64 = [int64]([int64]$LastHostOctetsArray[0]*16777216 +
+                            [int64]$LastHostOctetsArray[1]*65536 +
+                            [int64]$LastHostOctetsArray[2]*256 +
+                            ([int64]$LastHostOctetsArray[3]-1))
+          }
+          else {
+            $LastHostI64 = [int64]([int64]$LastHostOctetsArray[0]*16777216 +
+                            [int64]$LastHostOctetsArray[1]*65536 +
+                            [int64]$LastHostOctetsArray[2]*256 +
+                            ([int64]$LastHostOctetsArray[3]))
+          }            
+          $LastHostIP = ConvertFromINT64ToIP($LastHostI64)
+          $NicObject.LastHost = $LastHostIP | Write-Output
+        }
+      }
+      $Alt2NicObjects = Get-NetIPConfiguration
+      Foreach ($Alt2NicObject in $Alt2NicObjects){
+        if ($NicObject.InterfaceIndex -eq $Alt2NicObject.InterfaceIndex){
+          #Obtain Default Gateway
+          $IPv4DefaultGateway = Get-NetRoute | Where-Object -FilterScript {$_.ifIndex -eq $NicObject.InterfaceIndex} | Where-Object -FilterScript {$_.NextHop -Ne "::"} | Where-Object -FilterScript { $_.NextHop -Ne "0.0.0.0" }  | Where-Object -FilterScript { ($_.NextHop.SubString(0,6) -Ne 
+            "fe80::") } | Select-Object NextHop
+          $NicObject.IPv4DefaultGateway = $IPv4DefaultGateway.NextHop
+          #Obtain MacAddress
+          $MacAddress = Get-NetAdapter | Where-Object -FilterScript {$_.ifIndex -eq $NicObject.InterfaceIndex} | Select-Object MacAddress
+          $NicObject.MacAddress = $MacAddress -replace '.*=' -replace '}.*'
+      }
+    }
+  }
+}
+if ($t){
+###Print a formatted output for all Interfaces
+  $NicObjects | Format-Table InterfaceIndex,InterfaceAlias,MacAddress,IPv4Address,PrefixLength,SubnetMask,IPv4DefaultGateway,IPNetwork,SuffixOrigin,AddressState,FirstHost,LastHost -AutoSize | Out-String -Width 4096
+}
+else {
+  return $NicObjects
+}
 
-#$NicObjects | Write-Host
 
+<###Select a single Interface to print a formatted output
   Write-Host "Menu : Please Select from the following"
   Write-Host "e.g. 1"
   Foreach ($NicObject in $NicObjects){
@@ -134,8 +164,10 @@ function Get-PrimaryIPv4Information(){
   }
   [uInt16]$k=Read-Host -Prompt "Enter a number to select"
   if ($k -gt 0){$k--}
-  $NicObjects[$k] | Format-Table InterfaceIndex,InterfaceAlias,MacAddress,IPv4Address,PrefixLength,SubnetMask,IPv4DefaultGateway,IPNetwork,SuffixOrigin,AddressState,FirstHost,LastHost
+  $NicObjects[$k] | Format-Table InterfaceIndex,InterfaceAlias,MacAddress,IPv4Address,PrefixLength,SubnetMask,IPv4DefaultGateway,IPNetwork,SuffixOrigin,AddressState,FirstHost,LastHost -AutoSize | Out-String -Width 4096
+#>
 }
 
 #Main Script Starts From Here
 Export-ModuleMember -Function Get-PrimaryIPv4Information
+#Get-PrimaryIPv4Information -t -a
